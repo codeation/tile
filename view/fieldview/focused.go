c4bffae57c0cd321e1c44975dc2de70a5d2b52a7
codeation <39458623+codeation@users.noreply.github.com>
2025-11-09 02:15:45 -0800

    Add FieldWithoutFocused

--- view/fieldview/focused.go
+++ view/fieldview/focused.go
@@ -4,13 +4,13 @@ import (
 	"github.com/codeation/tile/view/fn"
 )
 
-// FieldWithFocused contains a field with focused state func
+// FieldWithFocused contains a field with focused state func.
 type FieldWithFocused struct {
 	Fielder
 	isFocused fn.Bool
 }
 
-// WithFocused returns a FieldWithFocused
+// WithFocused returns a FieldWithFocused.
 func WithFocused(field Fielder, isFocused fn.Bool) *FieldWithFocused {
 	return &FieldWithFocused{
 		Fielder:   field,
@@ -20,3 +20,18 @@ func WithFocused(field Fielder, isFocused fn.Bool) *FieldWithFocused {
 
 // Focused returns a focused flag
 func (m *FieldWithFocused) Focused() bool { return m.isFocused() }
+
+// FieldWithoutFocused contains a field with Focused always equal to False.
+type FieldWithoutFocused struct {
+	Fielder
+}
+
+// WithoutFocused returns a FieldWithoutFocused.
+func WithoutFocused(field Fielder) *FieldWithFocused {
+	return &FieldWithFocused{
+		Fielder: field,
+	}
+}
+
+// Focused returns a focused flag.
+func (m *FieldWithoutFocused) Focused() bool { return false }
