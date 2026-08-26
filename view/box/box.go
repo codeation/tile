a1e5c5f4b1a850c3f8563b8e5cafe6a5ddf8a281
codeation <39458623+codeation@users.noreply.github.com>
2026-08-26 22:17:07 +0000

    Modify view elements

--- view/box/box.go
+++ view/box/box.go
@@ -10,31 +10,58 @@ import (
 	"github.com/codeation/tile/view/fn"
 )
 
-// BoxView adds a border and background color to any Viewer
+// BoxView adds a border, background color, or padding to any Viewer.
 type BoxView struct {
-	view.Viewer
+	viewer     view.Viewer
+	padding    image.Point
 	foreground fn.Color
 	background fn.Color
 }
 
-// New returns a BoxView for specified Viewer. Foreground is a border color, Background is a new background color for a Viewer
-func New(viewer view.Viewer, foreground fn.Color, background fn.Color) *BoxView {
+// New returns a BoxView for specified Viewer.
+// Foreground is a border color, Background is a new background color for a Viewer.
+func New(viewer view.Viewer) *BoxView {
 	return &BoxView{
-		Viewer:     viewer,
-		foreground: foreground,
-		background: background,
+		viewer: viewer,
 	}
 }
 
-// Draw draws a element in a window width specified offset
-func (v *BoxView) Draw(w *impress.Window, from image.Point) {
-	boxRect := image.Rectangle{Min: from, Max: from.Add(v.Size())}
-	w.Fill(boxRect, v.background())
-	border.Border(w, boxRect, v.foreground())
-	v.Viewer.Draw(w, from)
+// Padding sets padding size function.
+func (v *BoxView) Padding(padding image.Point) *BoxView {
+	v.padding = padding
+	return v
 }
 
-// Select returns active element and its rect for the click point
+// Foreground sets func for border foreground color.
+func (v *BoxView) Foreground(foreground fn.Color) *BoxView {
+	v.foreground = foreground
+	return v
+}
+
+// Background sets func for box foreground color.
+func (v *BoxView) Background(background fn.Color) *BoxView {
+	v.background = background
+	return v
+}
+
+// Size returns size of a view element.
+func (v *BoxView) Size() image.Point {
+	return v.viewer.Size().Add(v.padding.Mul(2))
+}
+
+// Draw draws a element in a window width specified offset.
+func (v *BoxView) Draw(w *impress.Window, from image.Point) {
+	boxRect := image.Rectangle{Min: from, Max: from.Add(v.viewer.Size().Add(v.padding.Mul(2)))}
+	if v.background != nil {
+		w.Fill(boxRect, v.background())
+	}
+	if v.foreground != nil {
+		border.Border(w, boxRect, v.foreground())
+	}
+	v.viewer.Draw(w, from.Add(v.padding))
+}
+
+// Select returns active element and its rect for the click point.
 func (v *BoxView) Select(pt image.Point, from image.Point) (any, image.Rectangle) {
-	return v.Viewer.Select(pt, from)
+	return v.viewer.Select(pt, from.Add(v.padding))
 }
--- view/fn/fn.go
+++ view/fn/fn.go
@@ -1,14 +1,14 @@
 package fn
 
-// Const returns a function with a constant value of any type
+// Const returns a function with a constant value of any type.
 func Const[T any](v T) func() T { return func() T { return v } }
 
-// If returns composition of condFn
-func If[T any](condFn Bool, left, right func() T) func() T {
+// If creates a func that returns one of its parameters depending on result of condFn func.
+func If[T any](condFn Bool, left, right T) func() T {
 	return func() T {
 		if condFn() {
-			return left()
+			return left
 		}
-		return right()
+		return right
 	}
 }
